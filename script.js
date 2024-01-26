6465b2528a59323938c55eec0c6e41f90d6199cf
larisaa07 <larisa_aveti@yahoo.com>
2024-01-26 15:00:16 -0500

    js done

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
+//function to change the date, text, short blurb, and image to February 7, 1812
 function feb1812(){
-    
+    document.getElementById('date').innerText= 'February 7, 1812:'
+    document.getElementById('text').innerText= 'New text goes here'
+    document.getElementById('blurb').innerText= 'Charles Dickens was born'
+    document.getElementById('image').src= '/imgs/CharlesDickens.jpg'
+}
+
+//function to change the date, text, short blurb, and image to February 7, 1964
+function feb1964(){
+    document.getElementById('date').innerText= 'February 7, 1964:'
+    document.getElementById('text').innerText= 'New text goes here'
+    document.getElementById('blurb').innerText= 'The Beatles arrived in America causing Beatlemania'
+    document.getElementById('image').src= '/imgs/TheBeatles.jpg'
+}
+
+//function to change the date, text, short blurb, and image to February 7, 1984
+function feb1984(){
+    document.getElementById('date').innerText= 'February 7, 1984:'
+    document.getElementById('text').innerText= 'New text goes here'
+    document.getElementById('blurb').innerText= 'Bruce McCandless became the first person to spacewalk without a safety tether connecting him to a spacecraft'
+    document.getElementById('image').src= '/imgs/SpaceWalk.webp'
+}
+
+//function to change the date, text, short blurb, and image to February 7, 2007
+function feb2007(){
+    document.getElementById('date').innerText= 'February 7, 2007:'
+    document.getElementById('text').innerText= 'Text about this event'
+    document.getElementById('blurb').innerText= 'My Birthday!'
+    document.getElementById('image').src= '/imgs/SpaceWalk.webp'
+}
+
+//function to change the date, text, short blurb, and image to February 7, 2010
+function feb2010(){
+    document.getElementById('date').innerText= 'February 7, 2010:'
+    document.getElementById('text').innerText= 'New text goes here'
+    document.getElementById('blurb').innerText= 'First National US Tea Party Convention happened'
+    document.getElementById('image').src= '/imgs/TeaPartyConvention.jpg'
+}
+
+//function to change the date, text, short blurb, and image to February 7, 2022
+function feb2022(){
+    document.getElementById('date').innerText= 'February 7, 2022:'
+    document.getElementById('text').innerText= 'New text goes here'
+    document.getElementById('blurb').innerText= 'Americans were told that Russia would invade Ukraine “any day now” by President Biden’s national security advisor, Jake Sullivan'
+    document.getElementById('image').src= '/imgs/RussiaVSUkraine.jpg'
 }
