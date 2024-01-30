57b97d77d7ac167412889e5684d477b2aa0c79ad
larisaa07 <larisa_aveti@yahoo.com>
2024-01-30 13:48:53 -0500

    almost done

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //function to change the date, text, short blurb, and image to February 7, 1812
 function feb1812(){
     document.getElementById('date').innerText= 'February 7, 1812:'
-    document.getElementById('text').innerText= 'New text goes here'
+    document.getElementById('text').innerText= "Charles Dickens was born on February 7, 1812, in Portsmouth, England. He was the second of eight children. The Dickens familys financial struggles deeply influenced Charles's early life and greatly shaped the themes of poverty and social injustice that would later permeate his literary works. Despite the challenges, Dickens showed early signs of literary talent and a love for storytelling, foreshadowing the remarkable career that would unfold for him as one of the Victorian era's most celebrated authors."
     document.getElementById('blurb').innerText= 'Charles Dickens was born'
     document.getElementById('image').src= '/imgs/CharlesDickens.jpg'
 }
@@ -9,7 +9,7 @@ function feb1812(){
 //function to change the date, text, short blurb, and image to February 7, 1964
 function feb1964(){
     document.getElementById('date').innerText= 'February 7, 1964:'
-    document.getElementById('text').innerText= 'New text goes here'
+    document.getElementById('text').innerText= "The Beatles' arrival in America on February 7, 1964 marked a cultural phenomenon known as Beatlemania. The Fab Four, comprised of John Lennon, Paul McCartney, George Harrison, and Ringo Starr, landed to an exuberant crowd of fans and journalists. The Beatles' appearance on 'The Ed Sullivan Show' on February 9 catapulted them to unprecedented fame in the United States. Their energetic performances, catchy tunes, and charming personalities captured the hearts of American teenagers, sparking a frenzy of fanatical enthusiasm. Beatlemania swept the nation, with screaming fans, sold-out concerts, and a tidal wave of Beatles merchandise."
     document.getElementById('blurb').innerText= 'The Beatles arrived in America causing Beatlemania'
     document.getElementById('image').src= '/imgs/TheBeatles.jpg'
 }
@@ -17,7 +17,7 @@ function feb1964(){
 //function to change the date, text, short blurb, and image to February 7, 1984
 function feb1984(){
     document.getElementById('date').innerText= 'February 7, 1984:'
-    document.getElementById('text').innerText= 'New text goes here'
+    document.getElementById('text').innerText= "Bruce McCandless made history on February 7, 1984, by becoming the first person to perform a spacewalk without the safety of a tether. McCandless ventured away from the safety of the space shuttle Challenger during the STS-41-B mission. The iconic image of McCandless untethered, with Earth as a backdrop, symbolized human triumph over the challenges of space exploration and emphasized the incredible strides in astronaut mobility and technology. McCandless's daring spacewalk marked a significant milestone in the exploration of outer space and inspired future generations of astronauts and space enthusiasts."
     document.getElementById('blurb').innerText= 'Bruce McCandless became the first person to spacewalk without a safety tether connecting him to a spacecraft'
     document.getElementById('image').src= '/imgs/SpaceWalk.webp'
 }
@@ -25,7 +25,7 @@ function feb1984(){
 //function to change the date, text, short blurb, and image to February 7, 2007
 function feb2007(){
     document.getElementById('date').innerText= 'February 7, 2007:'
-    document.getElementById('text').innerText= 'Text about this event'
+    document.getElementById('text').innerText= 'On Februabr 7, 2007 Larisa Avetisyan was born to Armen Avetisyan and Ashkhen Tadevosyan. She now has a younger sister and two younger brothers. She is currently 16 years old and is a junior at Marlboro High School.'
     document.getElementById('blurb').innerText= 'My Birthday!'
     document.getElementById('image').src= '/imgs/SpaceWalk.webp'
 }
@@ -33,7 +33,7 @@ function feb2007(){
 //function to change the date, text, short blurb, and image to February 7, 2010
 function feb2010(){
     document.getElementById('date').innerText= 'February 7, 2010:'
-    document.getElementById('text').innerText= 'New text goes here'
+    document.getElementById('text').innerText= "The first National Tea Party Convention took place on February 7, 2010, in Nashville, Tennessee. The convention aimed to bring together activists from across the United States who shared common concerns about government spending, taxation, and individual liberties. The event included workshops, panel discussions, and speeches focusing on conservative principles and grassroots activism. However, the convention was not without controversy, as some criticized it for its for-profit nature and exclusivity, highlighting internal debates within the Tea Party movement at the time."
     document.getElementById('blurb').innerText= 'First National US Tea Party Convention happened'
     document.getElementById('image').src= '/imgs/TeaPartyConvention.jpg'
 }
@@ -41,7 +41,7 @@ function feb2010(){
 //function to change the date, text, short blurb, and image to February 7, 2022
 function feb2022(){
     document.getElementById('date').innerText= 'February 7, 2022:'
-    document.getElementById('text').innerText= 'New text goes here'
+    document.getElementById('text').innerText= "In a statement, President Biden's national security advisor, Jake Sullivan, informed Americans that there was a looming threat of a Russian invasion of Ukraine, heightening concerns. Sullivan's warning created a sense of urgency and prompted increased attention to the delicate geopolitical situation. The statement indicated the seriousness with which the United States viewed the potential conflict and emphasized the need for vigilance. The situation underscored the complex dynamics and ongoing challenges in international relations."
     document.getElementById('blurb').innerText= 'Americans were told that Russia would invade Ukraine “any day now” by President Biden’s national security advisor, Jake Sullivan'
     document.getElementById('image').src= '/imgs/RussiaVSUkraine.jpg'
 }
