79866f44bc670a2948f5847b91a653f94b8490f7
larisaa07 <larisa_aveti@yahoo.com>
2024-02-01 14:45:05 -0500

    chng img name

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function feb2007(){
     document.getElementById('date').innerText= 'February 7, 2007:'
     document.getElementById('text').innerText= "On Februabr 7, 2007 Larisa Avetisyan was born to Armen Avetisyan and Ashkhen Tadevosyan. She now has a younger sister and two younger brothers. She is currently 16 years old and is a junior at Marlboro High School."
     document.getElementById('blurb').innerText= 'My Birthday!'
-    document.getElementById('image').src= 'imgs/Me+Flowers.jpeg'
+    document.getElementById('image').src= 'imgs/MeFlowers.jpeg'
 }
 
 //function to change the date, text, short blurb, and image to February 7, 2010
